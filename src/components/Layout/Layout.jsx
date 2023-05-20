1bafa6908f741cc55fd0db2e9986658337e7e941
KatlegoMakofane <87699446+KatlegoMakofane@users.noreply.github.com>
2023-05-20 09:42:05 +0200

    first commit

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react'
+import Header from '../Header/Header'
+import Routers from '../../routers/Routers'
+import Footer from '../Footer/Footer'
+const Layout = () => {
+  return <Fragment>
+   
+      <Header/>
+        <div>
+          <Routers/>
+        </div>
+        <Footer/>
+    
+  </Fragment>
+}
+
+export default Layout
